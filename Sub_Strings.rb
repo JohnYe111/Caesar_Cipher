a7655471ff5e487a7fd001afaee16fb67ae7931c
JohnYe111 <yejohn30111@gmail.com>
2024-10-07 21:39:25 -0700

    Finish Sub Strings

--- Sub_Strings.rb
+++ Sub_Strings.rb
@@ -1,7 +1,24 @@
 def substrings(str, dictionary)
-    puts dictionary
+    res = {}
+    str = (str.gsub(/[^A-Za-z\s]/, '')).split
+    
+    dictionary.each do |s|
+        str.each do |word|
+            if (word.downcase).include?((s.downcase))
+                if res.key?(s)
+                    res[s] += 1
+                else
+                    res[s] = 1
+                end
+            end
+        end
+    end
+
+    return res
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
