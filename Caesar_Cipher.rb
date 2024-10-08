e70e6af1c1de4622de3af8f0512c93f28d8470f8
JohnYe111 <yejohn30111@gmail.com>
2024-10-07 19:59:22 -0700

    Finish caesar

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,4 +1,4 @@
-def caesar_cipher(str, leaps)
+def caesar_cipher(str, shifts)
     cap_min_bound = 65  # A
     cap_max_bound = 90  # Z
     low_min_bound = 97  # a
@@ -9,11 +9,11 @@ def caesar_cipher(str, leaps)
     low_table = {}
     res = ""
 
-    cap_min_bound.upto(cap_max_bound). each do |i|
+    cap_min_bound.upto(cap_max_bound).each do |i|
         cap_table[i] = i.chr
     end
 
-    low_min_bound.upto(low_max_bound). each do |i|
+    low_min_bound.upto(low_max_bound).each do |i|
         low_table[i] = i.chr
     end
     
@@ -21,26 +21,16 @@ def caesar_cipher(str, leaps)
         key = i.ord
         # capital letter
         if (key >= cap_min_bound && key <= cap_max_bound)
-            if key + leaps > cap_max_bound
-                res += cap_table[(key + leaps) - (leaps / 26) - 26]
-            else
-                res += cap_table[key + leaps]
-            end
+            res += cap_table[cap_min_bound + ((key - cap_min_bound + shifts) % 26)]
         # lower case letter
         elsif (key >= low_min_bound && key <= low_max_bound)
-            if key + leaps > low_max_bound
-                res += low_table[(key + leaps) - (leaps / 26) - 26]
-            else
-                res += low_table[key + leaps]
-            end
+            res += low_table[low_min_bound + ((key - low_min_bound + shifts) % 26)]
         else
             res += i
         end
     end
 
-    puts res
-
+    return res
 end
 
-
-puts (87 + 5) % 26
+puts caesar_cipher("What a string!", 5)
