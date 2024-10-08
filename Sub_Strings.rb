fe2584e8a40beba3cac45bbbc2bffd36e37b6f24
JohnYe111 <yejohn30111@gmail.com>
2024-10-07 20:08:33 -0700

    working on function

---
+++ Sub_Strings.rb
@@ -0,0 +1,7 @@
+def substrings(str, dictionary)
+    puts dictionary
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
